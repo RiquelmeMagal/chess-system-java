39ab7051220aa6efc3e8f7f2fd76f2bee896e933
Riquelme Magalhães <riquelme.souza@arapiraca.ufal.br>
2023-09-07 20:36:35 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Olá mundo.");
-
+		
+		Position p1 = new Position(3, 5);
+		
+		System.out.println(p1);
 	}
 
 }
