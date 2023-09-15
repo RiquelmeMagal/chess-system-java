c9edc4d04e58a752382ed46ac036000761fe8a0d
Riquelme Magalhães <riquelme.souza@arapiraca.ufal.br>
2023-09-15 13:05:53 -0300

    Placing pieces on the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,7 +1,6 @@
 package boardgame;
 
 public class Board {
-	
 	private int rows;
 	private int columns;
 	private Piece[][] pieces;
@@ -11,19 +10,15 @@ public class Board {
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
 	}
-
 	public int getRows() {
 		return rows;
 	}
-
 	public void setRows(int rows) {
 		this.rows = rows;
 	}
-
 	public int getColumns() {
 		return columns;
 	}
-
 	public void setColumns(int columns) {
 		this.columns = columns;
 	}
@@ -35,5 +30,9 @@ public class Board {
 	public Piece piece(Position position) {
 		return pieces[position.getRow()][position.getColumn()];
 	}
-	
+
+	public void placePiece(Piece piece, Position position) {
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
+	}
 }
